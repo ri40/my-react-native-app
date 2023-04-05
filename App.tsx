8a827ced0d9263905d01a486c86b26aef396987c
RimahAlorainiq <r.aloraini@thegarage.sa>
2023-04-05 12:59:13 +0300

    first comminte

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hi bashayer</Text>
     </View>
   );
 }
@@ -11,8 +12,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "pink",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
